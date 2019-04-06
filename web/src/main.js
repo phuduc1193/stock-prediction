b9d0c823103e482da7e25992b2981e2ea49ed040
Duc Truong <phuduc1193@gmail.com>
2019-04-06 14:32:10 -0500

    Add vue ui package

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css'
+
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+
+import './styles/index.scss'
+
 import App from './App'
 import router from './router'
+
+import i18n from './lang'
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 Vue.config.productionTip = false
 
