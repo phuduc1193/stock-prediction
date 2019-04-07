a3f212b65f0f7abbd6763d42b070b91711ca2855
Duc Truong <phuduc1193@gmail.com>
2019-04-06 23:27:19 -0500

    Add vue element admin template

--- web/src/lang/index.js
+++ web/src/lang/index.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import Cookies from 'js-cookie'
 import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
+import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN'// element-ui lang
+import elementEsLocale from 'element-ui/lib/locale/lang/es'// element-ui lang
 import enLocale from './en'
+import zhLocale from './zh'
+import esLocale from './es'
 
 Vue.use(VueI18n)
 
@@ -10,9 +14,16 @@ const messages = {
   en: {
     ...enLocale,
     ...elementEnLocale
+  },
+  zh: {
+    ...zhLocale,
+    ...elementZhLocale
+  },
+  es: {
+    ...esLocale,
+    ...elementEsLocale
   }
 }
-
 export function getLanguage() {
   const chooseLanguage = Cookies.get('language')
   if (chooseLanguage) return chooseLanguage
@@ -27,7 +38,6 @@ export function getLanguage() {
   }
   return 'en'
 }
-
 const i18n = new VueI18n({
   // set locale
   // options: en | zh | es
--- web/src/main.js
+++ web/src/main.js
@@ -1,32 +1,46 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 
 import Cookies from 'js-cookie'
 
-import 'normalize.css/normalize.css'
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import Element from 'element-ui'
 import './styles/element-variables.scss'
 
-import './styles/index.scss'
+import '@/styles/index.scss' // global css
 
 import App from './App'
+import store from './store'
 import router from './router'
 
-import i18n from './lang'
+import i18n from './lang' // Internationalization
+import './icons' // icon
+import './permission' // permission control
+import './utils/errorLog' // error log
+
+import * as filters from './filters' // global filters
+
+import { mockXHR } from '../mock' // simulation data
+
+// mock api in github pages site build
+if (process.env.NODE_ENV === 'production') { mockXHR() }
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
 })
 
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  i18n,
+  render: h => h(App)
 })
